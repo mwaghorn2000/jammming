21351e4378fd1fcfb2000bea16d156f963af1edb
unknown <m.waghorn2000@gmail.com>
2022-10-25 21:15:41 +1100

    add boilerplate

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App-playlist">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
